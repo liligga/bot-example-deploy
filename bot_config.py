4fc29610325310992a92b4af3031f24da0c05851
Igor <ig.g.like@gmail.com>
2024-08-26 00:39:55 +0600

    Prep for deploy

--- bot_config.py
+++ bot_config.py
@@ -5,15 +5,36 @@ from database.database import Database
 
 
 load_dotenv()
-token = getenv("BOT_TOKEN")
-bot = Bot(token=token)
-dp = Dispatcher()
 database = Database("db.sqlite3")
+
+# token = getenv("BOT_TOKEN")
+# bot = Bot(token=token)
+# dp = Dispatcher()
+
+dev = getenv("DEV", 0)
+if not dev:
+    from aiogram.client.session.aiohttp import AiohttpSession
+
+    print("started on server")
+    print(getenv("BOT_TOKEN"))
+    session = AiohttpSession(proxy=getenv("PROXY"))
+    bot = Bot(token=getenv("BOT_TOKEN"), session=session)
+
+else:
+    print("started on dev")
+    bot = Bot(token=getenv("BOT_TOKEN"))
+
+
+dp = Dispatcher()
 
 
 async def set_bot_commands():
-    await bot.set_my_commands([
-        types.BotCommand(command="start", description="Начало"),
-        types.BotCommand(command="picture", description="Получите картику"),
-        types.BotCommand(command="shop", description="Наш каталог книг")
-    ])
+    await bot.set_my_commands(
+        [
+            types.BotCommand(command="start", description="Начало"),
+            types.BotCommand(
+                command="picture", description="Получите картику"
+            ),
+            types.BotCommand(command="shop", description="Наш каталог книг"),
+        ]
+    )
